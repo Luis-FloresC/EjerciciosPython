78246e2f8e589ccf8e25a2fb95b3f6c63b28de46
Luis Flores <lf016158@gmail.com>
2022-03-15 14:59:36 -0600

    Ciclo for,conversion dato,potencias y a nivel de bit

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,7 +1,31 @@
-print("Hola Mundo")
-dato = input("Ingrese un Valor: ")
 
-for i in 10:
-    print(i)
+from operator import xor
+from pickle import TRUE
 
-print(type(dato))
+
+try:
+    print("Hola Mundo")
+    dato = input("Ingrese un Valor: ")
+
+    for i in range(int(dato)):
+        print(i)
+
+    print(type(dato))
+    potencia = "La potencia de {0} es: {1}"
+    sumaComplex = "( {0} + {1} ) = {2}"
+    MultoComplex = "( {0} * {1} ) = {2}"
+    a = 4j + int(dato)
+    b = 2j + int(dato)
+    print(sumaComplex.format(a,b,a+b))
+    print(MultoComplex.format(a,b,a*b))
+    print(xor(True,False))
+
+    if int(dato) > 5 :
+        print("mayor: ",a)
+    else:
+        print("Mayor: ",b)    
+    print(potencia.format(dato,int(dato)**2))
+    print(2&6)
+except:
+  print("Ocurrió un error")
+
