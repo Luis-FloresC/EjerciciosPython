0bc31bd37e8aa4f63d046ad2efd2d27290ca874c
Luis Flores <lf016158@gmail.com>
2022-03-24 14:57:25 -0600

    Metodo join y split

---
+++ Test6.py
@@ -0,0 +1,12 @@
+from shlex import join
+from tokenize import String
+
+
+
+ls = ["Hola","Mundo","Loco"]
+lsString = '-'.join(ls)
+print(lsString)
+
+listaStr = lsString.split('-')
+print(listaStr)
+print(join(ls,' '))
