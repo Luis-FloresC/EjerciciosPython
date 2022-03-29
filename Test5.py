cbd7483797564dc5c6e96629907b4d1756d7ba1d
Luis Flores <lf016158@gmail.com>
2022-03-29 12:17:53 -0600

    Se termino el ejercicio 1

--- Test5.py
+++ Test5.py
@@ -3,5 +3,11 @@
 listaAbecedario = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
                            "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
       
-listaAbecedario.sort(reverse=True)
-print(listaAbecedario)      
+def invertir_cadena_manual(cadena):
+    cadena_invertida = ""
+    for letra in cadena:
+        cadena_invertida = letra + cadena_invertida
+    return cadena_invertida
+
+
+print(invertir_cadena_manual("XYZ"))      
